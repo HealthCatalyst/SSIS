e04b644a85466a4280e88f0be9177cc3eab3c5f6
mdpettit <marshall.pettit@healthcatalyst.com>
2017-01-19 15:45:09 -0700

    Update ETLEngineConfigurationBASE seed script
    remove redundant slash and specify new extensibility point

--- SeedETLEngineConfigurationBASE.sql
+++ SeedETLEngineConfigurationBASE.sql
@@ -1,2 +1,2 @@
 INSERT INTO CatalystAdmin.ETLEngineConfigurationBASE (ExtensionPointNM, DatamartID, SSISPackagePathOrSPToExecuteTXT, IsSSISPackageFLG, ExtensionOwnerNM, RunInAsynchModeFLG, Use32BitRuntimeFLG, ExecutionOrderNBR, ActiveFLG, RequiredParametersTXT, FailsBatchFLG)
-VALUES ('OnPostTableLoad',{SAM datamart ID},'\\SSISDB\CatalystExtensibility\ExternalScriptExtensibility\ExternalScriptExecution.dtsx',1,'Health Catalyst',1,0,1,1,'BatchID, TableID',1)
+VALUES ('OnPostStageToProdLoad',{SAM datamart ID},'\SSISDB\CatalystExtensibility\ExternalScriptExtensibility\ExternalScriptExecution.dtsx',1,'Health Catalyst',1,0,1,1,'BatchID, TableID',1)
