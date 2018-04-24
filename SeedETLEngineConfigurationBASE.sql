0cca74d87bffaf7d7b7fe0c19c3b8dbe2e8d37ae
Levi Thatcher <levithatcher@users.noreply.github.com>
2018-04-24 13:19:59 -0600

    Obsolete, now that we have inserts in the doc

--- SeedETLEngineConfigurationBASE.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO CatalystAdmin.ETLEngineConfigurationBASE (ExtensionPointNM, DatamartID, SSISPackagePathOrSPToExecuteTXT, IsSSISPackageFLG, ExtensionOwnerNM, RunInAsynchModeFLG, Use32BitRuntimeFLG, ExecutionOrderNBR, ActiveFLG, RequiredParametersTXT, FailsBatchFLG)
-VALUES ('OnPostStageToProdLoad',{SAM datamart ID},'\SSISDB\CatalystExtensibility\ExternalScriptExtensibility\ExternalScriptExecution.dtsx',1,'Health Catalyst',1,0,1,1,'BatchID, TableID',1)
